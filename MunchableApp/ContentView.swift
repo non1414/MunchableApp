e4ce7a84ec5e449c3e4e390c2bd2c5ad70e1954c
non1414 <nouf22228@gmail.com>
2025-09-02 12:29:58 +0300

    Initial Commit

---
+++ MunchableApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MunchableApp
+//
+//  Created by نوف بخيت الغامدي on 10/03/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MunchableApp/MunchableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchableAppApp.swift
+//  MunchableApp
+//
+//  Created by نوف بخيت الغامدي on 10/03/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MunchableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
